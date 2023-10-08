6f5dc9c62ddc26b90b2f9785c59fcfc7ccde1b2e
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2023-10-08 03:01:20 +0000

    Update AuthOptions.jsx

--- app/components/AuthOptions.jsx
+++ app/components/AuthOptions.jsx
@@ -18,7 +18,9 @@ export default function AuthOptions() {
             <SignedOut>
                 <div className="flex flex-row items-center justify-center gap-8">
                     <div className="btn bg-[#3C77F7] px-[1.5vw] p-[1vw] flex items-center justify-center text-[#E6E6E6]">
-                        <SignInButton />
+                        <SignInButton mode="modal">
+                            <p className="items-center justify-center text-[#E6E6E6]">Sign In</p>
+                        </SignInButton>
                     </div>
                 </div> 
             </SignedOut>
