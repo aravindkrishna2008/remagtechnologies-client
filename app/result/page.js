cfb9305b122368395be56d2f88ab2e76057946d4
aravindkrishna2008 <aravindkrishna.nambi@gmail.com>
2023-10-07 18:36:37 -0700

    simple but suttle changes

--- app/result/page.js
+++ app/result/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthOptions from "../components/AuthOptions";
 
 export default function Home() {
   return (
@@ -11,30 +12,23 @@ export default function Home() {
           className="w-[20vw]"
         />
         <div className="flex flex-row items-center justify-center gap-8">
-          <p className="text-[#ABABAB]">Log In</p>
-          <div className="btn bg-[#3C77F7] px-[1.5vw] p-[1vw] flex items-center justify-center text-[#E6E6E6]">
-            Sign Up
-          </div>
+          <AuthOptions />
         </div>
       </div>
       <div className="flex flex-col items-center justify-center mt-[3vh]">
         <h1 className="title text-[4vw] leading-[4.2vw] flex items-center justify-center font-bold text-center p-4 w-[55vw]">
           Your reconnection analysis results.
         </h1>
-        <img src="/line1.png" className="w-[27vw] -mt-5 mr-[15vw]" />
+        <img src="/Line1.png" className="w-[27vw] -mt-5 mr-[15vw]" />
         <div className="gray-btn h-[18vh] w-[20vw] mr-[32vw] flex flex-col items-center justify-center -mt-[50px]">
           hi
         </div>
-        <img src="/line2.png" className="w-[40vw] -mt-[16vh] ml-[20vw]" />
+        <img src="/Line2.png" className="w-[40vw] -mt-[16vh] ml-[20vw]" />
         <div className="gray-btn h-[18vh] w-[20vw] mr-[32vw] flex flex-col items-center justify-center ml-[80vw] -mt-[80px]">
           hi
         </div>
-        <img src="/line1.png" className="w-[27vw] -mt-5 mr-[15vw]" />
-        <div className="gray-btn h-[18vh] w-[20vw] mr-[32vw] flex flex-col items-center justify-center -mt-[50px]">
-          hi
-        </div>
-        <img src="/line2.png" className="w-[40vw] -mt-[16vh] ml-[20vw]" />
-        <div className="gray-btn h-[18vh] w-[20vw] mr-[32vw] flex flex-col items-center justify-center ml-[80vw] -mt-[80px]">
+        <img src="/Line3.png" className="w-[18vw] -mt-[10vh] ml-[10vw]" />
+        <div className="gray-btn h-[18vh] w-[20vw] mr-[32vw] flex flex-col items-center justify-center -mt-[20vh] ml-[8vw]">
           hi
         </div>
       </div>
--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -1,6 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 import React from "react";
- 
+
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex flex-col h-[100vh] items-center justify-center ">
+      <SignUp />;
+    </div>
+  );
 }
