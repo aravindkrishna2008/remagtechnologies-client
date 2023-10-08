6ec2fce54c950562ffba5795ddd236c846cd377d
aravindkrishna2008 <aravindkrishna.nambi@gmail.com>
2023-10-08 16:21:17 -0700

    fixed some bugs in hoverfx

---
+++ app/globaldata.js
@@ -0,0 +1,6 @@
+// function to store data so it can be used in all files
+
+import React, { useState, createContext } from "react";
+
+const [data, setData] = useState();
+
--- app/result/page.js
+++ app/result/page.js
@@ -80,7 +80,7 @@ export default function Home() {
             </div>
             <div className="flex flex-row items-end gap-2">
               <h1 className="data-nums">0.212</h1>
-              <p className="text-[#666] mb-[5%] text-[1vw]">km2 per second</p>
+              <p className="text-[#666] mb-[5%] text-[1vw]">(unitless)</p>
             </div>
           </div>
         ) : (
@@ -114,7 +114,67 @@ export default function Home() {
         )}
 
         <img src="/Line2.png" className="w-[40vw] -mt-[16vh] ml-[20vw]" />
-        <div className="gray-btn h-[18vh] w-[22.5vw] mr-[37.5vw] flex flex-col p-[2vw] pb-[2vw] ml-[80vw] -mt-[80px] justify-center">
+        {!bzHover ? (
+          <div
+            className="gray-btn h-[18vh] w-[22.5vw] mr-[37.5vw] flex flex-col p-[2vw] pb-[2vw] ml-[80vw] -mt-[80px] justify-center"
+            onMouseLeave={() => {
+              setTimeout(() => {
+                setBzHover(false);
+              }, 1000);
+            }}
+          >
+            <div className="flex flex-row w-[100%] gap-[2px] items-center justify-center mt-[1.6%]">
+              <img src="/magnet.png" height={100} className="w-[15%]" />
+              <h2 className="text-[#B3B3B3] text-[1.2vw] w-[100%] font-bold mb-[2.5%]">
+                Bz (IMF Magnitude)
+              </h2>
+              <img
+                src="/question.png"
+                height={100}
+                className="w-[12%]"
+                onMouseEnter={() => setBzHover(true)}
+                onMouseLeave={() => {
+                  setTimeout(() => {
+                    setBzHover(false);
+                  }, 2000); // Wait for 4 seconds (4000 milliseconds) before setting graphHover to false
+                }}
+              />
+            </div>
+            <div className="flex flex-row items-end gap-2">
+              <h1 className="data-nums">-1.212</h1>
+              <p className="text-[#666] mb-[5%] text-[1vw]">nanoTeslas</p>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="gray-btn h-[23vh] w-[22.5vw] mr-[37.5vw] flex flex-col p-[2vw] pb-[2vw] ml-[80vw] -mt-[80px] justify-center"
+            onMouseEnter={() => setBzHover(true)}
+          >
+            <div className="flex flex-row w-[100%] gap-[2px] items-center justify-center mt-[1.6%]">
+              <img src="/magnet.png" height={100} className="w-[15%]" />
+              <h2 className="text-[#B3B3B3] text-[1.2vw] w-[100%] font-bold mb-[2.5%]">
+                Reconnection Rate
+              </h2>
+              <img
+                src="/question.png"
+                height={100}
+                className="w-[12%]"
+                onMouseEnter={() => setBzHover(true)}
+                onMouseLeave={() => {
+                  setTimeout(() => {
+                    setBzHover(false);
+                  }, 0); // Wait for 4 seconds (4000 milliseconds) before setting graphHover to false
+                }}
+              />
+            </div>
+            <p className=" group-hover:transition group-hover:ease-in-out text-[#666]  0 mb-1 text-[1vw]">
+              The Reconnection Rate measures the merging of the Geomagnetic
+              field and IMF in an indefinite cycle. A low Reconnection Rate can
+              allow solar particles to penetrate Earth's atmosphere.
+            </p>
+          </div>
+        )}
+        {/* <div className="gray-btn h-[18vh] w-[22.5vw] mr-[37.5vw] flex flex-col p-[2vw] pb-[2vw] ml-[80vw] -mt-[80px] justify-center">
           <div className="flex flex-row w-[100%] gap-[2px] items-center justify-center mt-[1.6%]">
             <img src="/magnet.png" height={100} className="w-[15%]" />
             <h2 className="text-[#B3B3B3] text-[1.2vw] w-[100%] font-bold mb-[2.5%]">
@@ -131,11 +191,14 @@ export default function Home() {
             }
             opt2={
               <p className="group-hover:transition group-hover:ease-in-out text-[#666] mb-[20%] text-[1vw]">
-                Hello World 2
+                The vertical component of the Interplanetary Magnetic Field
+                (IMF) is typically oriented upwards when positive, but flips
+                when negative, indicating the field's strength in that
+                direction.
               </p>
             }
           />
-        </div>
+        </div> */}
 
         <img src="/Line3.png" className="w-[18vw] -mt-[10vh] ml-[8vw]" />
         {!graphHover ? (
